03715735bb2751d874c6e09f2897626d12ae3731
vincentdman <65156350+vincentdman@users.noreply.github.com>
2021-03-28 22:05:18 +0200

    Commit after big cleanup

---
+++ main/main.cpp
@@ -0,0 +1,72 @@
+/**
+ * @file main.cpp
+ * @author Vincent de Man
+ * @brief Main application for S4 Project (.cpp)
+ * @version 0.1
+ * @date 2021-03-24
+ * 
+ * @copyright Copyright (c) 2021
+ * @details main.cpp main code for project S4  
+ */
+#include <stdio.h>
+#include <iostream>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include <driver/gpio.h>
+#include <driver/adc.h>
+#include <esp_wifi.h>
+#include "HandleGPIO.h"
+#include "RGB_Control.h"
+#include "PWM_GPIO.h"
+
+extern "C" void app_main(void)
+{
+
+   std::cout << "Hello world!\n";
+
+   //PWM_GPIO a;
+
+   //a.SetDuty(40);
+   // a.PWM_Initialize(GPIO_NUM_26);
+   // a.PWM_Initialize(GPIO_NUM_25);
+   //a.PWM_Initialize(GPIO_NUM_27);
+
+   //while (1)
+   // {
+   //a.SetDuty(0,GPIO_NUM_25);
+   //a.SetDuty(34,GPIO_NUM_27);
+   // for (int i=0; i < 100; i += 10)
+   // {
+   // a.SetDuty(i,GPIO_NUM_26);
+
+   //  a.UpdateTimerConfig();
+   //  vTaskDelay(1000 / portTICK_PERIOD_MS);
+   // }
+   // }
+   //HandleGPIO a;
+   //a.SetHigh(GPIO_NUM_4);
+   RGB_Control RGB_LED(GPIO_NUM_25, GPIO_NUM_26, GPIO_NUM_27);
+
+   RGB_LED.SetRGB(100, 50, 0);
+   /*
+   while (1)
+   {
+      RGB_LED.SetRed();
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      RGB_LED.SetBlue();
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      RGB_LED.SetGreen();
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      RGB_LED.SetPurple();
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      RGB_LED.SetCyan();
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      RGB_LED.SetYellow();
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      RGB_LED.SetWhite();
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+   }
+   */
+}
---
+++ main/src/PWM_GPIO.cpp
@@ -0,0 +1,88 @@
+/**
+ * @file PWM_GPIO.cpp
+ * @author Vincent de Man
+ * @brief Basic gpio PWM dirver class (.cpp)
+ * @version 0.1
+ * @date 2021-03-24
+ * @copyright Copyright (c) 2021
+ * @class PWM_GPIO Advanced class to give PWM functionality to a object
+ * 
+ */
+#include "driver/ledc.h"
+#include "esp_err.h"
+#include "esp_system.h"
+#include <driver/gpio.h>
+#include "PWM_GPIO.h"
+
+extern "C"
+{
+    /**
+     * @brief Construct a new pwm gpio::pwm gpio object
+     * 
+     */
+    PWM_GPIO::PWM_GPIO()
+    {
+    }
+    /**
+     * @brief Construct a new pwm gpio::pwm gpio object and initializes the given parameter
+     * 
+     * @param PIN The designated pin 
+     */
+    PWM_GPIO::PWM_GPIO(gpio_num_t PIN)
+    {
+        PWM_Initialize(PIN);
+    }
+    /**
+     * @brief function to set the PWM frequency 
+     * 
+     * @param Frequency The desired PWM Frequency
+     * @param PIN The designated pin 
+     */
+    void PWM_GPIO::SetFrequency(uint32_t Frequency, gpio_num_t PIN)
+    {
+        ledc_timer.freq_hz = Frequency;
+        PWM_Initialize(PIN);
+    }
+    /**
+     * @brief function to update the Channel configuration
+     * 
+     */
+    void PWM_GPIO::UpdateChannelConfig()
+    {
+        ledc_channel_config(&ledc_channel);
+    }
+    /**
+     * @brief function to update the Timer Configuration
+     * 
+     */
+    void PWM_GPIO::UpdateTimerConfig()
+    {
+        ledc_timer_config(&ledc_timer);
+    }
+    /**
+     * @brief function to set the duty cycle of the pwm signal
+     * 
+     * @param duty The desired duty cycle
+     * @param PIN The designated pin 
+     * @param Channel The affected pwm channel
+     */
+    void PWM_GPIO::SetDuty(int duty, gpio_num_t PIN, ledc_channel_t Channel)
+    {
+        //duty in range 0 to 8191
+        duty = duty * 8191 / 100;
+        ledc_channel.channel = Channel;
+        ledc_channel.duty = duty;
+        PWM_Initialize(PIN);
+    }
+    /**
+     * @brief function to initialize the given pin for pwm signal output
+     * 
+     * @param PIN The pin that gets initialized
+     */
+    void PWM_GPIO::PWM_Initialize(gpio_num_t PIN)
+    {
+        ledc_channel.gpio_num = PIN;
+        ledc_timer_config(&ledc_timer);
+        ledc_channel_config(&ledc_channel);
+    }
+}
