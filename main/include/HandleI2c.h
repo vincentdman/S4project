37dd5c65e27d33ab546849026519b60d745bff77
vincentdman <65156350+vincentdman@users.noreply.github.com>
2021-04-25 15:31:35 +0200

    Added doxygen

---
+++ main/include/HandleI2c.h
@@ -0,0 +1,19 @@
+/**
+ * @file HandleI2c.h
+ * @author Vincent de Man
+ * @brief Header file for HandleI2c
+ * @version 0.1
+ * @date 2021-04-25
+ * @details 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef HandleI2c_H
+#define HandleI2c_H
+
+
+
+
+
+#endif
---
+++ main/src/HandleI2c.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file HandleI2c.cpp
+ * @author Vincent de Man
+ * @brief Source file for HandleI2c.cpp
+ * @version 0.1
+ * @date 2021-04-25
+ * @details
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "driver/i2c.h"
+
+
+
+
+
