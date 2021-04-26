9324ef716b0246f901a70b748408c769e8c81d5a
vincentdman <65156350+vincentdman@users.noreply.github.com>
2021-04-26 19:42:12 +0200

    lcd control refactoring

--- main/include/LCD_Control.h
+++ main/include/LCD_Control.h
@@ -21,39 +21,57 @@ extern "C"
     class LCD_Control : private HandleGPIO
     {
     public:
+      typedef enum
+            {  
+                NotSelected = -1,
+                FourBitMode = 0,
+                EightBitMode = 1,
+                
+            }BitMode_t;
 
         typedef struct{
             gpio_num_t _RS = GPIO_NUM_NC;
             gpio_num_t _E = GPIO_NUM_NC; 
+            gpio_num_t _D0 = GPIO_NUM_NC;
+            gpio_num_t _D1 = GPIO_NUM_NC;
+            gpio_num_t _D2 = GPIO_NUM_NC;
+            gpio_num_t _D3 = GPIO_NUM_NC;
             gpio_num_t _D4 = GPIO_NUM_NC;
             gpio_num_t _D5 = GPIO_NUM_NC; 
             gpio_num_t _D6 = GPIO_NUM_NC; 
             gpio_num_t _D7 = GPIO_NUM_NC;
+            BitMode_t _BitMode = NotSelected;
         }LCD_Pinout_t;
 
         LCD_Control(LCD_Pinout_t &LCD_Pinout_Configuration);
 
 
-        LCD_Control(gpio_num_t _RS, gpio_num_t _E, gpio_num_t _D0, gpio_num_t _D1, gpio_num_t _D2, gpio_num_t _D3, gpio_num_t _D4, gpio_num_t _D5, gpio_num_t _D6, gpio_num_t _D7);
-        LCD_Control(gpio_num_t _RS, gpio_num_t _E, gpio_num_t _D4, gpio_num_t _D5, gpio_num_t _D6, gpio_num_t _D7);
-        ~LCD_Control();
         void LCD_Write_Char(char Data);
         void LCD_Initialize();
         void LCD_Write_Command(int Command);
         void LCD_Write_String(std::string Data_String_);
         void LCD_Write(std::string BinaryString, int RS);
 
+
+
+        void LCD_WriteFourBitMode(std::string BinaryString, int RS);
+        void LCD_WriteEightBitMode(std::string BinaryString, int RS);
+
     private:
-        int BitMode = EightBitMode;
+        void LCD_InitializeForSendingData(int RS);
+        void LCD_CycleDataTrough(void);
+        void LCD_SetDataEightBitMode(std::string BinaryString);
+        void LCD_SetDataFourBitModeFirstHalf(std::string BinaryString);
+        void LCD_SetDataFourBitModeSecondHalf(std::string BinaryString);
+
+
+
         enum
         {
-            RegisterSet = 0,
-            WriteAction = 1,
-            FourBitMode,
-            EightBitMode
-        };
+          RegisterSet = 0,
+           WriteAction };
         gpio_num_t LCD_RS = GPIO_NUM_NC;
-        gpio_num_t LCD_E = GPIO_NUM_NC;
+        gpio_num_t LCD_E  = GPIO_NUM_NC;
 
         gpio_num_t LCD_D0 = GPIO_NUM_NC;
         gpio_num_t LCD_D1 = GPIO_NUM_NC;
@@ -63,6 +81,9 @@ extern "C"
         gpio_num_t LCD_D5 = GPIO_NUM_NC;
         gpio_num_t LCD_D6 = GPIO_NUM_NC;
         gpio_num_t LCD_D7 = GPIO_NUM_NC;
+        
+        BitMode_t BitMode;
+
     };
 }
 #endif
--- main/src/LCD_Control.cpp
+++ main/src/LCD_Control.cpp
@@ -20,58 +20,13 @@
 
 extern "C"
 {
-    /**
-     * @brief Construct a new lcd control::lcd control object for 8 bit mode
-     * 
-     * @param _RS The register select pin of the lcd
-     * @param _E  The enable pin of the lcd
-     * @param _D0 The first data pin of the lcd
-     * @param _D1 The second data pin of the lcd
-     * @param _D2 The third data pin of the lcd
-     * @param _D3 The fourth data pin of the lcd
-     * @param _D4 The fith data pin of the lcd
-     * @param _D5 The sixt data pin of the lcd
-     * @param _D6 The seventh data pin of the lcd
-     * @param _D7 The eight data pin of the lcd
-     * 
-     * @details constructor for the use of 8 bit mode lcd. The class gets initialized for eight bit mode.
-     */
-    LCD_Control::LCD_Control(gpio_num_t _RS, gpio_num_t _E, gpio_num_t _D0, gpio_num_t _D1, gpio_num_t _D2, gpio_num_t _D3, gpio_num_t _D4, gpio_num_t _D5, gpio_num_t _D6, gpio_num_t _D7)
-        : LCD_RS(_RS), LCD_E(_E), LCD_D0(_D0), LCD_D1(_D1), LCD_D2(_D2), LCD_D3(_D3), LCD_D4(_D4), LCD_D5(_D5), LCD_D6(_D6), LCD_D7(_D7)
-    {
-        BitMode = EightBitMode;
-        LCD_Initialize();
-    }
 
-    /**
-     * @brief Construct a new lcd control::lcd control object for 4 bit mode
-     * 
-     * @param _RS The Register select pin of the lcd
-     * @param _E  The enable pin of the lcd
-     * @param _D4 The fifth data pin of the lcd
-     * @param _D5 The sixth data pin of the lcd
-     * @param _D6 The seventh data pin of the lcd
-     * @param _D7 The eight data pin of the lcd
-     * 
-     * @details constructor for the use of 4 bit mode lcd. The class gets initialized for four bit mode.
-     */
-    LCD_Control::LCD_Control(gpio_num_t _RS, gpio_num_t _E, gpio_num_t _D4, gpio_num_t _D5, gpio_num_t _D6, gpio_num_t _D7)
-        : LCD_RS(_RS), LCD_E(_E), LCD_D4(_D4), LCD_D5(_D5), LCD_D6(_D6), LCD_D7(_D7)
+    LCD_Control::LCD_Control(LCD_Pinout_t &LCD_Pinout_Configuration)
+        : LCD_RS(LCD_Pinout_Configuration._RS), LCD_E(LCD_Pinout_Configuration._E), LCD_D0(LCD_Pinout_Configuration._D0), LCD_D1(LCD_Pinout_Configuration._D1),
+          LCD_D2(LCD_Pinout_Configuration._D2), LCD_D3(LCD_Pinout_Configuration._D3), LCD_D4(LCD_Pinout_Configuration._D4), LCD_D5(LCD_Pinout_Configuration._D5),
+          LCD_D6(LCD_Pinout_Configuration._D6), LCD_D7(LCD_Pinout_Configuration._D7), BitMode(LCD_Pinout_Configuration._BitMode)
     {
-        BitMode = FourBitMode;
         LCD_Initialize();
-    }
-    /**
-     * @brief Destroy the lcd control::lcd control object
-     * 
-     * @details Destructor to set the output of the pins back to zero after destruction of the object. 
-     * Mainly to remove any glitches when shutting down and restarting
-     */
-    LCD_Control::~LCD_Control()
-    {
-        std::string lcd_off = "00000000";
-        LCD_Write(lcd_off, 0);
-        SetHigh(LCD_E);
     }
 
     /**
@@ -138,68 +93,79 @@ extern "C"
         InitializeGPIO(LCD_D0);
     }
 
-    /**
-     * @brief Writes a binary std string to the lcd.
-     * 
-     * @param BinaryString The binary string that will be set on the lcd pins.
-     * @param RS If the command needs to be written as data or register
-     * 
-     * @details Based on four or eight bit mode the function sets the binary string correct on the lcd pins and pulses the enable pin. 
-     * It also set the rs pin based on the input parameter RS. This will make sure that the correct mode is used. 
-     * The modes are register write and write to lcd. 
-     */
+
     void LCD_Control::LCD_Write(std::string BinaryString, int RS)
     {
-        switch (BitMode)
+        if (BitMode == FourBitMode)
         {
-        case FourBitMode:
-
-            ets_delay_us(1000);
-            gpio_set_level(LCD_RS, RS);
-            ets_delay_us(270);
-            SetHigh(LCD_E);
-            ets_delay_us(240);
-            gpio_set_level(LCD_D4, BinaryString[3] - '0');
-            gpio_set_level(LCD_D5, BinaryString[2] - '0');
-            gpio_set_level(LCD_D6, BinaryString[1] - '0');
-            gpio_set_level(LCD_D7, BinaryString[0] - '0');
-            ets_delay_us(300);
-            SetLow(LCD_E);
-            ets_delay_us(1000);
-
-            SetHigh(LCD_E);
-            ets_delay_us(240);
-            gpio_set_level(LCD_D4, BinaryString[7] - '0');
-            gpio_set_level(LCD_D5, BinaryString[6] - '0');
-            gpio_set_level(LCD_D6, BinaryString[5] - '0');
-            gpio_set_level(LCD_D7, BinaryString[4] - '0');
-            ets_delay_us(300);
-            SetLow(LCD_E);
-            ets_delay_us(1000);
-            break;
-
-        case EightBitMode:
-            ets_delay_us(1000);
-            gpio_set_level(LCD_RS, RS);
-            ets_delay_us(270);
-            SetHigh(LCD_E);
-            ets_delay_us(240);
-            gpio_set_level(LCD_D0, BinaryString[7] - '0');
-            gpio_set_level(LCD_D1, BinaryString[6] - '0');
-            gpio_set_level(LCD_D2, BinaryString[5] - '0');
-            gpio_set_level(LCD_D3, BinaryString[4] - '0');
-            gpio_set_level(LCD_D4, BinaryString[3] - '0');
-            gpio_set_level(LCD_D5, BinaryString[2] - '0');
-            gpio_set_level(LCD_D6, BinaryString[1] - '0');
-            gpio_set_level(LCD_D7, BinaryString[0] - '0');
-            ets_delay_us(300);
-            SetLow(LCD_E);
-            ets_delay_us(1000);
-            break;
-
-        default:
-            std::cout << "Bitmode error, No bitmode selected." << std::endl;
-            break;
+            LCD_WriteFourBitMode(BinaryString,RS);
         }
+        else{
+            LCD_WriteEightBitMode(BinaryString,RS);
+        }
+    }
+
+    void LCD_Control::LCD_WriteFourBitMode(std::string BinaryString, int RS)
+    {
+        LCD_InitializeForSendingData(RS);
+        LCD_SetDataFourBitModeFirstHalf(BinaryString);
+        LCD_CycleDataTrough();
+        LCD_SetDataFourBitModeSecondHalf(BinaryString);
+        LCD_CycleDataTrough();
+    }
+
+    void LCD_Control::LCD_WriteEightBitMode(std::string BinaryString, int RS)
+    {
+        LCD_InitializeForSendingData(RS);
+        LCD_SetDataEightBitMode(BinaryString);
+        LCD_CycleDataTrough();
+    }
+
+
+
+    void LCD_Control::LCD_InitializeForSendingData(int RS)
+    {
+        ets_delay_us(1000);
+        gpio_set_level(LCD_RS, RS);
+        ets_delay_us(270);
+        SetHigh(LCD_E);
+        ets_delay_us(240);
+    }
+
+    void LCD_Control::LCD_CycleDataTrough(void)
+    {
+        ets_delay_us(300);
+        SetLow(LCD_E);
+        ets_delay_us(1000);
+    }
+
+    void LCD_Control::LCD_SetDataEightBitMode(std::string BinaryString)
+    {
+        gpio_set_level(LCD_D0, BinaryString[7] - '0');
+        gpio_set_level(LCD_D1, BinaryString[6] - '0');
+        gpio_set_level(LCD_D2, BinaryString[5] - '0');
+        gpio_set_level(LCD_D3, BinaryString[4] - '0');
+        gpio_set_level(LCD_D4, BinaryString[3] - '0');
+        gpio_set_level(LCD_D5, BinaryString[2] - '0');
+        gpio_set_level(LCD_D6, BinaryString[1] - '0');
+        gpio_set_level(LCD_D7, BinaryString[0] - '0');
+    }
+
+    void LCD_Control::LCD_SetDataFourBitModeFirstHalf(std::string BinaryString)
+    {
+        gpio_set_level(LCD_D4, BinaryString[3] - '0');
+        gpio_set_level(LCD_D5, BinaryString[2] - '0');
+        gpio_set_level(LCD_D6, BinaryString[1] - '0');
+        gpio_set_level(LCD_D7, BinaryString[0] - '0');
+    }
+
+    void LCD_Control::LCD_SetDataFourBitModeSecondHalf(std::string BinaryString)
+    {
+        SetHigh(LCD_E);
+        ets_delay_us(240);
+        gpio_set_level(LCD_D4, BinaryString[7] - '0');
+        gpio_set_level(LCD_D5, BinaryString[6] - '0');
+        gpio_set_level(LCD_D6, BinaryString[5] - '0');
+        gpio_set_level(LCD_D7, BinaryString[4] - '0');
     }
 }
