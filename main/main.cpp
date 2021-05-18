74813dbad7528d9d32958610ad49a6a6d71bc344
vincentdman <65156350+vincentdman@users.noreply.github.com>
2021-05-18 16:52:31 +0200

    testing code to main.h

--- main/main.cpp
+++ main/main.cpp
@@ -12,9 +12,10 @@
 
  extern "C" void app_main(void)
  {
+   HandleJOYstick JOY();
 
-
-   while(1){
+   while(1)
+   {
 
    }
 
@@ -47,37 +48,3 @@
 
 
 
-//extern "C" void app_main(void)
-// {
-//   ESP_LOGI(TAG, "\n\nBuild version: %.2f\n", Build_Version);
-//   ESP_LOGI(TAG, "Created by Vincent de Man\n");
-
-//   LCD_Pinout_t Configs;
-//   Configs._BitMode = I2CMode;
-//   Configs._SDA = GPIO_NUM_21;
-//   Configs._SCL = GPIO_NUM_22;
-  
-
-//   HandleI2C I2c(true);
-//   LCD_Control LCD(Configs);
-//   RGB_Control LED(GPIO_NUM_23,GPIO_NUM_19,GPIO_NUM_4);
-//   HandleBMP180 BMP;
-
-//   LCD.LCD_Write_Command(LCD_DISPLAY_ON_CURSOR_OFF);
-//   LCD.LCD_Write_Command(LCD_CLEAR);
-
-
-//   while (1)
-//   {
-//     LCD.LCD_Write_Command(LCD_HOME);
-//     LCD.LCD_Write_String("Temperature:");
-//     LCD.LCD_Write_Float(BMP.GetTemperature());
-//     LCD.LCD_Write_Command(LCD_NEXT_LINE);
-    
-//     LCD.LCD_Write_String("Pressure: ");
-//     LCD.LCD_Write_Float(BMP.GetPressure());
-    
-//     LED.SetRGB(rand()%100,rand()%100,rand()%100);
-//     vTaskDelay(1000 / portTICK_PERIOD_MS);
-//   }
-// }
